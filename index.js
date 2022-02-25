851a7d9f8379dc4045b443d276f436a5ba2aeaa7
Jakub <malinowski.jakub@yahoo.com>
2022-02-25 16:59:26 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+let distance, someValue;
+const headquarters = 42;
+function distanceFromHqInBlocks(someValue) {
+    if (someValue >= headquarters){
+        distance = someValue - 42
+    } else if (someValue < headquarters){
+        distance = 42 - someValue
+    }
+    return distance;
+}
+
+let distanceInFeet;
+function distanceFromHqInFeet(distance) {
+    distanceInFeet = (distanceFromHqInBlocks(distance)) * 264;
+    return distanceInFeet;
+}
+
+let start, destination, distanceTravelFeet;
+function distanceTravelledInFeet(start, destination) {
+    if (destination >= start) {
+        distanceTravelFeet = (destination - start) * 264;
+    } else {
+        distanceTravelFeet = (start - destination) * 264;
+    }
+    return distanceTravelFeet;
+}
+
+let distanceCalculate, fare;
+function calculatesFarePrice(start, destination) {
+    if (destination >= start){
+        distanceCalculate = ((destination - start) * 264) - 400;
+    } else if (start >= destination){
+        distanceCalculate = ((start - destination) *264) - 400;
+    }
+    
+    if (distanceCalculate <= 0) {
+        fare = 0;
+    } else if ((distanceCalculate > 0) && (distanceCalculate <= 1600)) {
+        fare = distanceCalculate * 0.02;
+    } else if ((distanceCalculate >1600) && (distanceCalculate <= 2100)) {
+        fare = 25
+    } else if (distanceCalculate > 2100) {
+        return "cannot travel that far"
+    }
+    return fare;
+}
